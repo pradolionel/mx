abb77dfaf7adbf9742e2c6f7fd7a7bbdbdafd4cd
LionelMessiKB100 <99144412+pradolionel@users.noreply.github.com>
2024-08-03 17:56:45 -0500

    Update MainMenuState.hx

--- source/meta/state/menus/MainMenuState.hx
+++ source/meta/state/menus/MainMenuState.hx
@@ -1,8 +1,8 @@
-package meta.state.menus;
+ package meta.state.menus;
 
 import flixel.FlxBasic;
 import flixel.FlxG;
-import flixel.FlxKeyboard
+import flixel.FlxKeyboard;
 import flixel.FlxObject;
 import flixel.FlxSprite;
 import flixel.addons.display.FlxGridOverlay;
